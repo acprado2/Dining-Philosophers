8491b8a43c76e4e8d436bee8651ceb616d380c39
acprado <acprado@cpp.edu>
2018-10-15 05:36:56 -0700

    Added unstructured lock implementation
    + Fixed incorrect class names

---
+++ src/DiningPhilosopherStructured.java
@@ -0,0 +1,118 @@
+import java.util.Random;
+
+public class DiningPhilosopherStructured
+{
+	public static void main( String args[] )
+	{
+		Fork forkList[] = new Fork[5];
+		
+		for ( int i = 0; i < 5; i++ )
+		{
+			forkList[i] = new Fork( i );
+		}
+		
+		for ( int i = 0; i < 5; i++ )
+		{
+			if ( i != 4 )
+			{
+				Thread t = new Thread( new PhilosopherStructured( forkList[i], forkList[i + 1], i + 1 ) );
+				t.start();
+			}
+			else
+			{
+				// Fork to the right of the last philosopher should be the first fork
+				Thread t = new Thread( new PhilosopherStructured( forkList[i], forkList[0], i + 1 ) );
+				t.start();
+			}
+		}
+		
+	}
+}
+
+class PhilosopherStructured implements Runnable
+{
+	private Fork left;
+	private Fork right;
+	private String name;
+	
+	public PhilosopherStructured( Fork left, Fork right, int number )
+	{
+		this.left = left;
+		this.right = right;
+		this.name = "Philosopher " + number;
+	}
+	
+	public void run()
+	{
+		Random rand = new Random();
+		int seconds;
+		
+		// Figure out which fork is higher/lower
+		boolean bIsLeftHigher = left.GetNumber() > right.GetNumber() ? true : false;
+		
+		// Set our references
+		Fork higher = bIsLeftHigher ? left : right;
+		Fork lower = bIsLeftHigher ? right : left; 
+		
+		while( true )
+		{
+			// Philosopher will always grab the lower-numbered fork first to ensure deadlocks never occur
+			System.out.println( name + ": attempt to acquire fork to " + ( bIsLeftHigher ? "right" : "left" ) );
+			synchronized( lower )
+			{
+				System.out.println( name + ": acquired " + ( bIsLeftHigher ? "right" : "left" ) + " fork" );				
+				System.out.println( name + ": attempt to acquire fork to " + ( bIsLeftHigher ? "left" : "right" ) );
+				synchronized( higher )
+				{
+					System.out.println( name + ": acquired" + ( bIsLeftHigher ? "left" : "right" ) + "fork" );
+					right.Eat( name );
+				}
+			}
+			
+			// Finished eating. Think for a while
+			try
+			{
+				// Sleep for x seconds
+				seconds = rand.nextInt( 4 ) + 1;
+				System.out.println( name + ": Thinking for " + seconds + " seconds" );
+				Thread.sleep( 1000 * seconds );
+			} 
+			catch ( InterruptedException e ) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Fork
+{
+	private int number;
+	
+	public Fork( int number )
+	{
+		this.number = number;
+	}
+	
+	public void Eat( String name )
+	{
+		Random rand = new Random();
+		int seconds = rand.nextInt( 9 ) + 1;
+		
+		try 
+		{
+			// Sleep for x seconds
+			System.out.println( name + ": Eating for " + seconds + " seconds" );
+			Thread.sleep( 1000 * seconds );
+		} 
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public int GetNumber()
+	{
+		return number;
+	}
+}
--- src/DiningPhilosopherUnstructured.java
+++ src/DiningPhilosopherUnstructured.java
@@ -1,27 +1,29 @@
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class DiningPhilosopherUnstructured
+public class DiningPhilosopherUnstructured 
 {
 	public static void main( String args[] )
 	{
-		Fork forkList[] = new Fork[5];
+		Lock forkList[] = new ReentrantLock[5];
 		
 		for ( int i = 0; i < 5; i++ )
 		{
-			forkList[i] = new Fork( i );
+			forkList[i] = new ReentrantLock( );
 		}
 		
 		for ( int i = 0; i < 5; i++ )
 		{
 			if ( i != 4 )
 			{
-				Thread t = new Thread( new Philosopher( forkList[i], forkList[i + 1], i + 1 ) );
+				Thread t = new Thread( new PhilosopherUnstructured( forkList[i], forkList[i + 1], i + 1, false ) );
 				t.start();
 			}
 			else
 			{
 				// Fork to the right of the last philosopher should be the first fork
-				Thread t = new Thread( new Philosopher( forkList[i], forkList[0], i + 1 ) );
+				Thread t = new Thread( new PhilosopherUnstructured( forkList[i], forkList[0], i + 1, true ) );
 				t.start();
 			}
 		}
@@ -29,89 +31,131 @@ public class DiningPhilosopherUnstructured
 	}
 }
 
-class Philosopher implements Runnable
+class PhilosopherUnstructured implements Runnable
 {
-	private Fork left;
-	private Fork right;
-	private String name;
+	// Action states
+	enum States
+	{
+		STATE_EATING,
+		STATE_THINKING,
+		STATE_HUNGRY,
+	}
 	
-	public Philosopher( Fork left, Fork right, int number )
+	private Lock left;
+	private Lock right;
+	private String name;
+	private boolean bLast;
+	
+	public PhilosopherUnstructured( Lock left, Lock right, int number, boolean bIsLastPhilosopher )
 	{
 		this.left = left;
 		this.right = right;
 		this.name = "Philosopher " + number;
+		this.bLast = bIsLastPhilosopher;
 	}
 	
 	public void run()
-	{
-		Random rand = new Random();
-		int seconds;
-		
-		// Figure out which fork is higher/lower
-		boolean bIsLeftHigher = left.GetNumber() > right.GetNumber() ? true : false;
-		
-		// Set our references
-		Fork higher = bIsLeftHigher ? left : right;
-		Fork lower = bIsLeftHigher ? right : left; 
+	{	
+		// The last philosopher should have the first fork to his right
+		Lock higher = bLast ? left : right;
+		Lock lower = bLast ? right : left; 
 		
 		while( true )
 		{
+			boolean bGrabbedLower = false, bGrabbedHigher = false;
+			
 			// Philosopher will always grab the lower-numbered fork first to ensure deadlocks never occur
-			System.out.println( name + ": attempt to acquire fork to " + ( bIsLeftHigher ? "right" : "left" ) );
-			synchronized( lower )
+			System.out.println( name + ": attempt to acquire fork to " + ( bLast ? "right" : "left" ) );
+			
+			// Keep trying to grab the lower fork until we succeed
+			while ( !bGrabbedLower )
 			{
-				System.out.println( name + ": acquired " + ( bIsLeftHigher ? "right" : "left" ) + " fork" );				
-				System.out.println( name + ": attempt to acquire fork to " + ( bIsLeftHigher ? "left" : "right" ) );
-				synchronized( higher )
+				// Try to grab the lower-numbered fork
+				bGrabbedLower = lower.tryLock();
+				
+				if ( bGrabbedLower )
 				{
-					System.out.println( name + ": acquired" + ( bIsLeftHigher ? "left" : "right" ) + "fork" );
-					right.Eat( name );
+					// We got the fork
+					try
+					{
+						System.out.println( name + ": acquired " + ( bLast ? "right" : "left" ) + " fork" );				
+						System.out.println( name + ": attempt to acquire fork to " + ( bLast ? "left" : "right" ) );
+						
+						// Keep trying to grab the higher fork until we succeed
+						while ( !bGrabbedHigher )
+						{
+							// Try to grab the higher-numbered fork
+							bGrabbedHigher = higher.tryLock();
+							
+							if ( bGrabbedHigher )
+							{
+								// We got the fork
+								try
+								{
+									System.out.println( name + ": acquired " + ( bLast ? "left" : "right" ) + " fork" );
+									Think( States.STATE_EATING );
+								}
+								finally
+								{
+									higher.unlock();
+								}
+							}
+							else
+							{
+								// We didn't get the fork. Think for a while
+								System.out.println( name + ": Failed to aquire "  + ( bLast ? "left" : "right" ) + " fork" );		
+								Think( States.STATE_HUNGRY );
+							}
+						}
+					}
+					finally
+					{
+						lower.unlock();
+					}
 				}
+				else
+				{
+					// We didn't get the fork. Think for a while
+					System.out.println( name + ": Failed to aquire "  + ( bLast ? "right" : "left" ) + " fork" );		
+					Think( States.STATE_HUNGRY );
+				}
+			
 			}
 			
-			try
-			{
-				// Sleep for x seconds
-				seconds = rand.nextInt( 4 ) + 1;
-				System.out.println( name + ": Thinking for " + seconds + " seconds" );
-				Thread.sleep( 1000 * seconds );
-			} 
-			catch (InterruptedException e) 
-			{
-				e.printStackTrace();
-			}
+			// Finished eating. Think for a while
+			Think( States.STATE_THINKING );
 		}
 	}
-}
-
-class Fork
-{
-	private int number;
 	
-	public Fork( int number )
-	{
-		this.number = number;
-	}
-	
-	public void Eat( String name )
+	public void Think( States state )
 	{
 		Random rand = new Random();
-		int seconds = rand.nextInt( 9 ) + 1;
+		int seconds = 0;
 		
-		try 
-		{
-			// Sleep for x seconds
-			System.out.println( name + ": Eating for " + seconds + " seconds" );
+		try
+		{			
+			// Different actions depending on the state
+			switch( state )
+			{
+			case STATE_EATING:
+				seconds = rand.nextInt( 9 ) + 1;
+				System.out.println( name + ": Eating for " + seconds + " seconds" );
+				break;
+			case STATE_THINKING:
+				seconds = rand.nextInt( 4 ) + 1;
+				System.out.println( name + ": Thinking for " + seconds + " seconds" );
+				break;
+			case STATE_HUNGRY:
+				seconds = rand.nextInt( 4 ) + 1;
+				System.out.println( name + ": Thinks about how hungry he is for " + seconds + " seconds" );
+				break;
+			}
+			
 			Thread.sleep( 1000 * seconds );
 		} 
-		catch (InterruptedException e) 
+		catch ( InterruptedException e ) 
 		{
 			e.printStackTrace();
 		}
-	}
-	
-	public int GetNumber()
-	{
-		return number;
 	}
 }
